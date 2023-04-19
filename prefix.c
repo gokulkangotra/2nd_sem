d3d7b120c92143906a87e9a817a717ab558f068a
GOKUL KANGOTRA <122631207+gokulkangotra@users.noreply.github.com>
2023-04-19 14:32:23 +0530

    Add files via upload

---
+++ prefix.c
@@ -0,0 +1,70 @@
+//Program to Evaluate Prefix Expression
+#include<stdio.h>
+#include<string.h>
+
+int s[50];
+int top=0;
+
+void push(int ch);
+int pop();
+
+int main()
+{
+	int a,b,c,i;
+	char prefix[50];
+	printf("\nEnter the prefix string in figures(1 digit nos):");
+	gets(prefix);
+
+	//for(i=0;i<strlen(prefix);i++)
+	for(i=strlen(prefix)-1;i>=0;i--)
+	{
+		if(prefix[i]=='+')
+		{
+			c=pop()+pop();
+			push(c);
+		}
+		else if(prefix[i]=='-')
+		{
+			a=pop();
+			b=pop();
+			c=b-a;
+			push(c);
+		}
+		else if(prefix[i]=='*')
+		{	a=pop();
+		b=pop();
+		c=b*a;
+		push(c);
+		}
+		else if(prefix[i]=='/')
+		{
+			a=pop();
+			b=pop();
+			c=b/a;
+			push(c);
+		}
+		else
+		{
+			push(prefix[i]-48);
+			//printf("\n INT=%d - CHAR=%d",prefix[i]-48,c);
+		}
+	}
+	printf("\nFinal ans = %d",pop());
+
+	getch();
+	return 0;
+}
+
+void push(int ch)
+{
+	top++;
+	s[top]=ch;
+}
+
+int pop()
+{
+	int ch;
+	ch=s[top];
+	top=top-1;
+	return(ch);
+}
